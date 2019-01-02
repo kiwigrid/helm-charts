93edeb71cd319cdfbbdd9be7851eea6cbbc97232
André Bauer <andre.bauer@kiwigrid.com>
2019-01-02 17:53:39 +0100

    added repo root var to all bash scripts

--- .circleci/lint-charts.sh
+++ .circleci/lint-charts.sh
@@ -8,7 +8,10 @@ set -o pipefail
 
 CONFIG_DIR=".circleci"
 GIT_REPO="https://github.com/kiwigrid/helm-charts"
+REPO_ROOT="$(git rev-parse --show-toplevel)"
 
 git remote add k8s "${GIT_REPO}"
 git fetch k8s master
-ct lint --config="${CONFIG_DIR}"/ct.yaml --lint-conf="${CONFIG_DIR}"/lintconf.yaml --chart-yaml-schema="${CONFIG_DIR}"/chart_schema.yaml
+ct lint --config="${REPO_ROOT}/${CONFIG_DIR}"/ct.yaml \
+  --lint-conf="${REPO_ROOT}/${CONFIG_DIR}"/lintconf.yaml \
+  --chart-yaml-schema="${REPO_ROOT}/${CONFIG_DIR}"/chart_schema.yaml
--- .circleci/lint-scripts.sh
+++ .circleci/lint-scripts.sh
@@ -7,5 +7,6 @@ set -o errexit
 set -o pipefail
 
 CONFIG_DIR="./.circleci"
+REPO_ROOT="$(git rev-parse --show-toplevel)"
 
-find "${CONFIG_DIR}" -type f -name "*.sh" -exec echo lint {}\; -exec shellcheck -x {} \;
+find "${REPO_ROOT}/${CONFIG_DIR}" -type f -name "*.sh" -exec echo lint {}\; -exec shellcheck -x {} \;
