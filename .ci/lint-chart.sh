3ac8c79fa3ac25d351a8f80c59de63a8ea97f0d8
André Bauer <monotek23@gmail.com>
2019-12-04 23:09:05 +0100

    replace circle ci with github actions

--- .ci/lint-chart.sh
+++ .ci/lint-chart.sh
@@ -5,12 +5,13 @@
 
 set -o errexit
 
-CONFIG_DIR=".circleci"
+CONFIG_DIR=".ci"
 GIT_REPO="https://github.com/kiwigrid/helm-charts"
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 
 git remote add k8s "${GIT_REPO}"
 git fetch k8s master
+
 ct lint --config="${REPO_ROOT}/${CONFIG_DIR}"/ct.yaml \
   --lint-conf="${REPO_ROOT}/${CONFIG_DIR}"/lintconf.yaml \
   --chart-yaml-schema="${REPO_ROOT}/${CONFIG_DIR}"/chart_schema.yaml
