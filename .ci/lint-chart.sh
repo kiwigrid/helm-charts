3269ccf1b1c623d91bdc229f8dc2179c8a8d1132
André Bauer <andre.bauer@kiwigrid.com>
2019-12-05 18:07:34 +0100

    added chart dir option
    Signed-off-by: André Bauer <andre.bauer@kiwigrid.com>

--- .ci/lint-chart.sh
+++ .ci/lint-chart.sh
@@ -12,8 +12,7 @@ REPO_ROOT="$(git rev-parse --show-toplevel)"
 git remote add k8s "${GIT_REPO}"
 git fetch k8s master
 
-cd "${REPO_ROOT}"
-
 ct lint --config="${REPO_ROOT}/${CONFIG_DIR}"/ct.yaml \
   --lint-conf="${REPO_ROOT}/${CONFIG_DIR}"/lintconf.yaml \
-  --chart-yaml-schema="${REPO_ROOT}/${CONFIG_DIR}"/chart_schema.yaml
+  --chart-yaml-schema="${REPO_ROOT}/${CONFIG_DIR}"/chart_schema.yaml \
+  --chart-dirs="${REPO_ROOT}/charts"
