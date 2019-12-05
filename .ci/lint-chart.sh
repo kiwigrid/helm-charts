86aba00da14d63730b196d04405f1a85a95e2a1a
André Bauer <monotek23@gmail.com>
2019-12-05 23:54:02 +0100

    test
    Signed-off-by: André Bauer <monotek23@gmail.com>

--- .ci/lint-chart.sh
+++ .ci/lint-chart.sh
@@ -13,10 +13,13 @@ REPO_ROOT="$(git rev-parse --show-toplevel)"
 git remote add k8s "${GIT_REPO}"
 git fetch k8s master
 
-CHART="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/k8s/master -- charts | head -n 1 | sed -e 's#charts/##g' -e 's#/[cC]hart.yaml##g')"
+CHART="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/k8s/master -- charts | head -n 1 | sed -e 's#charts/##g' -e 's#/.*##g')"
 
 ct lint --config="${REPO_ROOT}/${CONFIG_DIR}"/ct.yaml \
   --lint-conf="${REPO_ROOT}/${CONFIG_DIR}"/lintconf.yaml \
   --chart-yaml-schema="${REPO_ROOT}/${CONFIG_DIR}"/chart_schema.yaml \
-  --charts="${REPO_ROOT}/charts/${CHART}"
+  --chart-dirs="${REPO_ROOT}/charts"
+  
+  
+  #--charts="${REPO_ROOT}/charts/${CHART}"
 
