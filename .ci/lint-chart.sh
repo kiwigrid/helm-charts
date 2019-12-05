fbbbb5df31557d90331966f1f3c99c8debc37d8c
André Bauer <andre.bauer@kiwigrid.com>
2019-12-05 18:12:47 +0100

    test
    Signed-off-by: André Bauer <andre.bauer@kiwigrid.com>

--- .ci/lint-chart.sh
+++ .ci/lint-chart.sh
@@ -4,6 +4,7 @@
 #
 
 set -o errexit
+set -x
 
 CONFIG_DIR=".ci"
 GIT_REPO="https://github.com/kiwigrid/helm-charts"
@@ -11,6 +12,9 @@ REPO_ROOT="$(git rev-parse --show-toplevel)"
 
 git remote add k8s "${GIT_REPO}"
 git fetch k8s master
+
+ls -al ${REPO_ROOT}
+ls -al ${REPO_ROOT}/charts
 
 ct lint --config="${REPO_ROOT}/${CONFIG_DIR}"/ct.yaml \
   --lint-conf="${REPO_ROOT}/${CONFIG_DIR}"/lintconf.yaml \
