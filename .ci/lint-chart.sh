b5c49d6403048347fc734c94309ae13db9f0cec4
André Bauer <monotek23@gmail.com>
2019-12-05 23:35:28 +0100

    test
    Signed-off-by: André Bauer <monotek23@gmail.com>

--- .ci/lint-chart.sh
+++ .ci/lint-chart.sh
@@ -13,7 +13,8 @@ REPO_ROOT="$(git rev-parse --show-toplevel)"
 git remote add k8s "${GIT_REPO}"
 git fetch k8s master
 
-git diff --find-renames --name-only $(git rev-parse --abbrev-ref HEAD) remotes/k8s/master -- charts
+# debug
+git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/k8s/master -- charts
 
 
 ct lint --config="${REPO_ROOT}/${CONFIG_DIR}"/ct.yaml \
