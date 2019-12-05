9c7f1f50c24290aed1b25b94d68dc8c2aa2fea69
André Bauer <andre.bauer@kiwigrid.com>
2019-12-05 18:18:54 +0100

    test
    Signed-off-by: André Bauer <andre.bauer@kiwigrid.com>

--- .ci/lint-chart.sh
+++ .ci/lint-chart.sh
@@ -13,10 +13,7 @@ REPO_ROOT="$(git rev-parse --show-toplevel)"
 git remote add k8s "${GIT_REPO}"
 git fetch k8s master
 
-ls -al "${REPO_ROOT}"
-ls -al "${REPO_ROOT}"/charts
-
 ct lint --config="${REPO_ROOT}/${CONFIG_DIR}"/ct.yaml \
   --lint-conf="${REPO_ROOT}/${CONFIG_DIR}"/lintconf.yaml \
   --chart-yaml-schema="${REPO_ROOT}/${CONFIG_DIR}"/chart_schema.yaml \
-  --chart-dirs="${REPO_ROOT}/charts/"
+
