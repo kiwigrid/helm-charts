b0e4d02dc1545e9c5ef3ab07cade5eeaacf97cb1
André Bauer <monotek23@gmail.com>
2019-12-06 11:48:24 +0100

    fixing ci
    Signed-off-by: André Bauer <monotek23@gmail.com>

--- .ci/lint-chart.sh
+++ .ci/lint-chart.sh
@@ -12,7 +12,11 @@ REPO_ROOT="$(git rev-parse --show-toplevel)"
 git remote add k8s "${GIT_REPO}"
 git fetch k8s master
 
-# workaround for ct chart detection 
+# workaround for ct chart detection
+if ! git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/k8s/master -- charts | grep -q '[cC]hart.yaml';then
+  echo "no Chart.yaml detected - do you raised the chart version?"
+  exit 1
+fi  
 CHART="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/k8s/master -- charts | head -n 1 | sed -e 's#charts/##g' -e 's#/.*##g')"
 
 ct lint --config="${REPO_ROOT}/${CONFIG_DIR}"/ct.yaml \
@@ -25,6 +29,3 @@ ct lint --config="${REPO_ROOT}/${CONFIG_DIR}"/ct.yaml \
 # ct lint --config="${REPO_ROOT}/${CONFIG_DIR}"/ct.yaml \
 #   --lint-conf="${REPO_ROOT}/${CONFIG_DIR}"/lintconf.yaml \
 #   --chart-yaml-schema="${REPO_ROOT}/${CONFIG_DIR}"/chart_schema.yaml 
-
-
-
