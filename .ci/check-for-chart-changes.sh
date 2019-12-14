3c6ac48e26620f44e6099a50c5375159cd58bb7b
André Bauer <monotek23@gmail.com>
2019-12-14 16:32:56 +0100

    debug
    Signed-off-by: André Bauer <monotek23@gmail.com>

--- .ci/check-for-chart-changes.sh
+++ .ci/check-for-chart-changes.sh
@@ -6,12 +6,19 @@
 set -o errexit
 set -o pipefail
 
+set -x
+ls -al
+
 CHART_REPO="https://github.com/kiwigrid/helm-charts.git"
+
+echo "Check for chart changes to speedup ci..."
 
 git remote add chart-changes "${CHART_REPO}"
 git fetch chart-changes master
 
 if [ -z "$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/chart-changes/master -- charts)" ]; then
-  echo "Error! No chart changes detected!"
+  echo "Error! No chart changes detected! Exiting..."
   exit 1
+else
+  echo "Changes found... Continue with next job..."
 fi
