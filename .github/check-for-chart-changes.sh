cb8eef7c5ba73515beaf486e4eb78abd7e95273c
André Bauer <monotek23@gmail.com>
2020-03-04 22:58:52 +0100

    changed chart changed detection
    Signed-off-by: André Bauer <monotek23@gmail.com>

--- .github/check-for-chart-changes.sh
+++ .github/check-for-chart-changes.sh
@@ -6,6 +6,8 @@
 set -o errexit
 set -o pipefail
 
+echo "Check for chart changes to speedup ci..."
+
 if [ -z "$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/master -- charts)" ]; then
   echo -e "\n\n Error! No chart changes detected! Exiting... \n"
   exit 1
