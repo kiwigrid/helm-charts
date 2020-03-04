d6ee7f49d11e624d7ca339b09106ead374a70573
André Bauer <monotek23@gmail.com>
2020-03-04 22:51:01 +0100

    debug
    Signed-off-by: André Bauer <monotek23@gmail.com>

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -6,10 +6,7 @@
 set -o errexit
 set -o pipefail
 
-ls -al
-git branch --all
-
-CHART_DIRS="$(git diff --name-only master | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
+CHART_DIRS="$(git diff --name-only remotes/origin/master | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
 
 for CHART_DIR in ${CHART_DIRS};do 
   echo "kubeval(idating) ${CHART_DIR##charts/} chart..."
