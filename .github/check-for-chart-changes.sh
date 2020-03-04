c042db933fa6a32efe76c9eff0766e8ceab6289e
André Bauer <monotek23@gmail.com>
2020-03-04 22:58:23 +0100

    changed chart changed detection
    Signed-off-by: André Bauer <monotek23@gmail.com>

--- .github/check-for-chart-changes.sh
+++ .github/check-for-chart-changes.sh
@@ -6,14 +6,7 @@
 set -o errexit
 set -o pipefail
 
-CHART_REPO="https://github.com/kiwigrid/helm-charts.git"
-
-echo "Check for chart changes to speedup ci..."
-
-git remote add chart-changes "${CHART_REPO}"
-git fetch chart-changes master
-
-if [ -z "$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/chart-changes/master -- charts)" ]; then
+if [ -z "$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/master -- charts)" ]; then
   echo -e "\n\n Error! No chart changes detected! Exiting... \n"
   exit 1
 else
